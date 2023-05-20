1540a66cf3f49a7cf251622bd793fde687d734fb
omarelbehairy <117220543+omarelbehairy@users.noreply.github.com>
2023-05-20 22:24:35 +0300

    Add files via upload

--- DSP_task4/GUImodified.py
+++ DSP_task4/GUImodified.py
@@ -7,7 +7,6 @@ import logging
 import numpy as np
 
 
-
 logger = logging.getLogger('my_logger')
 logger.setLevel(logging.DEBUG)
 
@@ -27,13 +26,18 @@ file_handler.setFormatter(formatter)
 # add file handler to logger
 logger.addHandler(file_handler)
 
+target_size = (500, 500)
 
-#customizing the layout
-st.set_page_config(layout="wide")
-flag=0
+# customizing the layout
+st.set_page_config(
+
+    layout="wide",
+)
+
+flag = 0
 left_column, right_column = st.columns(2)
 with left_column:
-    first_img_container_left  = st.container()
+    first_img_container_left = st.container()
     second_img_container_left = st.container()
 
 with right_column:
@@ -43,54 +47,70 @@ with right_column:
 with first_img_container_left:
     left_column, right_column = st.columns(2)
     with left_column:
-        uploaded_file1 = st.file_uploader("Choose an image1 file",type=['jpg','jpeg','png'])
+        uploaded_file1 = st.sidebar.file_uploader(
+            "Choose an image1 file", type=['jpg', 'jpeg', 'png'])
+        st.write("<p style='font-size:40px; text-align: center;'><b>Image 1</b></p>",
+                 unsafe_allow_html=True)
+
         logger.info('Uploading image 1...')
 
     with right_column:
-        option1 = st.selectbox("choose the desired image1 transformtion",('FT Magnitude','FT Phase','FT Real','FT Imaginary'))
+        option1 = st.selectbox("choose the desired image1 transformtion",
+                               ('FT Magnitude', 'FT Phase', 'FT Real', 'FT Imaginary'))
 
 with second_img_container_left:
     left_column, right_column = st.columns(2)
     with left_column:
-        uploaded_file2 = st.file_uploader("Choose an image2 file",type=['jpg','jpeg','png'])
+        uploaded_file2 = st.sidebar.file_uploader(
+            "Choose an image2 file", type=['jpg', 'jpeg', 'png'])
+        st.write("<p style='font-size:40px; text-align: center;'><b>Image 2</b></p>",
+                 unsafe_allow_html=True)
+
         logger.info('Uploading image 2...')
 
     with right_column:
-        option2 = st.selectbox("choose the desired image2 transformtion",('FT Magnitude','FT Phase','FT Real','FT Imaginary'))
+        option2 = st.selectbox("choose the desired image2 transformtion",
+                               ('FT Magnitude', 'FT Phase', 'FT Real', 'FT Imaginary'))
 
 with mixer_component:
-    output_option=st.selectbox("mixer output to",('output1','output2'))
+    output_option = st.selectbox("mixer output to", ('output1', 'output2'))
     left_column, right_column = st.columns(2)
     with left_column:
-        comp1=st.selectbox("component1",('img1','img2'))
+        comp1 = st.selectbox("component1", ('img1', 'img2'))
     with right_column:
-        mixrate1=st.slider("output1",0,100,0)
-    mix_comp1=st.selectbox("Mag,phase,real,imaginary,uniphase,uniMag",('Mag','phase','real','imaginary','uniphase','uniMag'))
+        mixrate1 = st.slider("output1", 0, 100, 0)
+    mix_comp1 = st.selectbox("Mag,phase,real,imaginary,uniphase,uniMag",
+                             ('Mag', 'phase', 'real', 'imaginary', 'uniphase', 'uniMag'))
     left_column1, right_column1 = st.columns(2)
     with left_column1:
-        comp2=st.selectbox("component2",('img1','img2'))
+        comp2 = st.selectbox("component2", ('img1', 'img2'))
     with right_column1:
-        mixrate2=st.slider("output2",0,100,0)
-    mix_comp2=st.selectbox("2Mag,phase,real,imaginary,uniphase,uniMag",('Mag','phase','real','imaginary','uniphase','uniMag'))
+        mixrate2 = st.slider("output2", 0, 100, 0)
+    mix_comp2 = st.selectbox("2Mag,phase,real,imaginary,uniphase,uniMag",
+                             ('Mag', 'phase', 'real', 'imaginary', 'uniphase', 'uniMag'))
 
 with result_img_container_right:
     left_column, right_column = st.columns(2)
 
-#importing style css.file
+# importing style css.file
+hide_st_style = """ <style> #MainMenu {visibility: hidden;} footer {visibility: hidden;}
+header {visibility: hidden;} </style>"""
+st.markdown(hide_st_style, unsafe_allow_html=True)
 with open("file.css") as fl:
-    st.markdown(f"<style>{fl.read()}</style>", unsafe_allow_html = True)
+    st.markdown(f"<style>{fl.read()}</style>", unsafe_allow_html=True)
 
 if uploaded_file1 is not None and uploaded_file2 is not None:
 
     file_size1 = uploaded_file1.size
     file_size2 = uploaded_file2.size
+
     if file_size1 != file_size2:
         logger.error('Error the two image not same size')
-        flag=1    
-    
+        flag = 1
+
     class TotalGui:
         def __init__(self):
-            
+
             self.path1 = uploaded_file1
             self.path2 = uploaded_file2
 
@@ -98,17 +118,22 @@ if uploaded_file1 is not None and uploaded_file2 is not None:
             self.image1 = self.image1.convert('L')  # convert to grayscale
             self.image1 = np.array(self.image1)
 
-
             self.image2 = Image.open(self.path2)
             self.image2 = self.image2.convert('L')  # convert to grayscale
             self.image2 = np.array(self.image2)
 
-            self.magnitudeSpec1,self.phaseSpec1,self.realSpec1,self.imagSpec1 = ap.modes_for_the_image(self.image1)
-            self.magnitudeSpec2,self.phaseSpec2,self.realSpec2,self.imagSpec2 = ap.modes_for_the_image(self.image2)
+            self.magnitudeSpec1, self.phaseSpec1, self.realSpec1, self.imagSpec1 = ap.modes_for_the_image(
+                self.image1)
+            self.magnitudeSpec2, self.phaseSpec2, self.realSpec2, self.imagSpec2 = ap.modes_for_the_image(
+                self.image2)
 
+            if 'tempimage' not in st.session_state:
+                st.session_state['tempimage'] = None
+            if 'tempimage2' not in st.session_state:
+                st.session_state['tempimage2'] = None
 
-            #self.fig1, (self.ax1, self.ax2) = plt.subplots(1, 2, figsize=(5,3))
-            #self.fig2, (self.ax12, self.ax22) = plt.subplots(1, 2, figsize=(5,3))
+            # self.fig1, (self.ax1, self.ax2) = plt.subplots(1, 2, figsize=(5,3))
+            # self.fig2, (self.ax12, self.ax22) = plt.subplots(1, 2, figsize=(5,3))
 
             self.logger = logging.getLogger('my_logger')
             self.logger.setLevel(logging.DEBUG)
@@ -116,7 +141,8 @@ if uploaded_file1 is not None and uploaded_file2 is not None:
             file_handler = logging.FileHandler('my_logs.txt')
             file_handler.setLevel(logging.DEBUG)
 
-            formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+            formatter = logging.Formatter(
+                '%(asctime)s - %(levelname)s - %(message)s')
 
             file_handler.setFormatter(formatter)
 
@@ -124,91 +150,93 @@ if uploaded_file1 is not None and uploaded_file2 is not None:
 
         def selectModes(self):
 
-            fig1, (ax1, ax2) = plt.subplots(1, 2, figsize=(3,2))
-            ax1 = plt.subplot(1,2,1)
+            fig1, (ax1, ax2) = plt.subplots(1, 2, figsize=(3, 2))
+            ax1 = plt.subplot(1, 2, 1)
             ax1.imshow(self.image1, cmap='gray')
             ax1.axis('off')
-            ax2 = plt.subplot(1,2,2)
+            ax2 = plt.subplot(1, 2, 2)
 
-            if(option1 == 'FT Magnitude'):
+            if (option1 == 'FT Magnitude'):
                 ax2.imshow(self.magnitudeSpec1, cmap='gray')
-            elif(option1 == 'FT Phase'):
+            elif (option1 == 'FT Phase'):
                 ax2.imshow(self.phaseSpec1, cmap='gray')
-            elif(option1 == 'FT Real'):
+            elif (option1 == 'FT Real'):
                 ax2.imshow(self.realSpec1, cmap='gray')
-            elif(option1 == 'FT Imaginary'):
+            elif (option1 == 'FT Imaginary'):
                 ax2.imshow(self.imagSpec1, cmap='gray')
             with first_img_container_left:
                 ax2.axis('off')
                 fig1.tight_layout()
                 st.pyplot(fig1)
 
-            fig2, (ax12, ax22) = plt.subplots(1, 2, figsize=(3,2))
-            ax12 = plt.subplot(1,2,1)
-            ax12.imshow(self.image2, cmap='gray')    
+            fig2, (ax12, ax22) = plt.subplots(1, 2, figsize=(3, 2))
+            ax12 = plt.subplot(1, 2, 1)
+            ax12.imshow(self.image2, cmap='gray')
             ax12.axis('off')
-            ax22 = plt.subplot(1,2,2)
+            ax22 = plt.subplot(1, 2, 2)
 
-            if(option2 == 'FT Magnitude'):
+            if (option2 == 'FT Magnitude'):
                 ax22.imshow(self.magnitudeSpec2, cmap='gray')
-            elif(option2 == 'FT Phase'):
+            elif (option2 == 'FT Phase'):
                 ax22.imshow(self.phaseSpec2, cmap='gray')
-            elif(option2 == 'FT Real'):
+            elif (option2 == 'FT Real'):
                 ax22.imshow(self.realSpec2, cmap='gray')
-            elif(option2 == 'FT Imaginary'):
+            elif (option2 == 'FT Imaginary'):
                 ax22.imshow(self.imagSpec2, cmap='gray')
             with second_img_container_left:
                 ax22.axis('off')
                 fig2.tight_layout()
                 st.pyplot(fig2)
 
-        def drawOP(self,Total_img):
-            if 'Total_img'  not in st.session_state:
-                st.session_state['Total_img'] = 'Total_img'
+        def drawOP(self, Total_img, output):
+
             if np.any(Total_img):
-                st.image(Total_img,use_column_width=True)
+                st.image(Total_img, use_column_width=True)
+
+                if output == 'output2':
+                    st.session_state.tempimage2 = Total_img
+                    if st.session_state.tempimage is not None and st.session_state.tempimage2 is not None: 
+                        with left_column:
+                            st.image(st.session_state.tempimage)
+                if output == 'output1':
+                    st.session_state.tempimage = Total_img
+                    if st.session_state.tempimage is not None and st.session_state.tempimage2 is not None: 
+                        with right_column:
+                            st.image(st.session_state.tempimage2)
             else:
-                st.write("not valid combination")  
+                st.write("not valid combination")
+
+        def output(self, output):
+            if ((comp1 != comp2) and (comp1 == 'img2')):
+                Total_img = ap.mix_photos(
+                    self.image2, self.image1, mixrate1, mixrate2, mix_comp1, mix_comp2)
+                self.drawOP(Total_img, output)
+            elif (comp1 == comp2 == 'img1'):
+                Total_img = ap.mix_photos(
+                    self.image1, self.image1, mixrate1, mixrate2, mix_comp1, mix_comp2)
+                self.drawOP(Total_img, output)
+            elif (comp1 == comp2 == 'img2'):
+                Total_img = ap.mix_photos(
+                    self.image2, self.image2, mixrate1, mixrate2, mix_comp1, mix_comp2)
+                self.drawOP(Total_img, output)
+            else:
+                Total_img = ap.mix_photos(
+                    self.image1, self.image2, mixrate1, mixrate2, mix_comp1, mix_comp2)
+                self.drawOP(Total_img, output)
 
         def adjust_output_location(self):
             if output_option == 'output1':
                 with result_img_container_right:
                     with left_column:
-                        if((comp1!=comp2)and(comp1=='img2')):
-                            Total_img = ap.mix_photos(self.image2,self.image1,mixrate1,mixrate2,mix_comp1,mix_comp2)
-                            self.drawOP(Total_img)  
-                        elif(comp1==comp2=='img1'):
-                            Total_img = ap.mix_photos(self.image1,self.image1,mixrate1,mixrate2,mix_comp1,mix_comp2)
-                            self.drawOP(Total_img)
-                        elif(comp1==comp2=='img2'):
-                            Total_img = ap.mix_photos(self.image2,self.image2,mixrate1,mixrate2,mix_comp1,mix_comp2)
-                            self.drawOP(Total_img)
-                        else:
-                            Total_img = ap.mix_photos(self.image1,self.image2,mixrate1,mixrate2,mix_comp1,mix_comp2)
-                            self.drawOP(Total_img)
+                        self.output('output1')
 
-            if output_option=='output2':
+            if output_option == 'output2':
                 with result_img_container_right:
                     with right_column:
-                        if((comp1!=comp2)and(comp1=='img2')):
-                            Total_img = ap.mix_photos(self.image2,self.image1,mixrate1,mixrate2,mix_comp1,mix_comp2)
-                            self.drawOP(Total_img)
-                        elif(comp1==comp2=='img1'):
-                            Total_img = ap.mix_photos(self.image1,self.image1,mixrate1,mixrate2,mix_comp1,mix_comp2)
-                            self.drawOP(Total_img)
-                        elif(comp1==comp2=='img2'):
-                            Total_img = ap.mix_photos(self.image2,self.image2,mixrate1,mixrate2,mix_comp1,mix_comp2)
-                            self.drawOP(Total_img)
-                        else:
-                            Total_img = ap.mix_photos(self.image1,self.image2,mixrate1,mixrate2,mix_comp1,mix_comp2)
-                            self.drawOP(Total_img)
-           
-
-
+                        self.output('output2')
 
     result = TotalGui()
     result.selectModes()
     logger.info('Displaying images different spectrum ...')
     result.adjust_output_location()
     logger.info('Mixing images ...')
-
--- DSP_task4/app4.py
+++ DSP_task4/app4.py
@@ -62,14 +62,33 @@ def produce_mix(image1,image2):
     phase2 = np.angle(image2_fft)
     phase_shift2 = np.angle(image2_fft_shift)
 
-    uniform_magnitude1 = np.ones(magnitude1)
-    uniform_phase1 = np.zeros(phase1)
+    uniform_magnitude1 = np.ones(magnitude1.shape)
+    uniform_phase1 = np.zeros(phase1.shape)
     
-    uniform_magnitude2 = np.ones(magnitude2)
-    uniform_phase2 = np.zeros(phase2)
+    uniform_magnitude2 = np.ones(magnitude2.shape)
+    uniform_phase2 = np.zeros(phase2.shape)
 
+    #st.write(uniform_magnitude2)
     return image1_fft,image2_fft,image1_fft_shift,image2_fft_shift,image1_real,image1_imaginary,image2_real,image2_imaginary,magnitude1,magnitude_shift1,magnitude2,magnitude_shift2,phase1,phase_shift1,phase2,phase_shift2,uniform_magnitude1,uniform_phase1,uniform_magnitude2,uniform_phase2 
 
+
+def Mag_phase_Mix(mixing_ratio1,mixing_ratio2,magnitude1,magnitude2,phase1,phase2):
+    mag_mix = magnitude1*(mixing_ratio1/100)+magnitude2*(1-(mixing_ratio1/100))
+    phase_mix = phase1*(1-(mixing_ratio2/100))+phase2*(mixing_ratio2/100)
+
+    combined_total = np.multiply(mag_mix, np.exp(1j * phase_mix))
+    
+    return np.real(np.fft.ifft2(combined_total))
+
+def Uniform_Mag_Phase(mixing_ratio1,mixing_ratio2,image1_real,image2_real,image1_imaginary,image2_imaginary):
+    mixed_real = image1_real * (mixing_ratio1 / 100) + image2_real * (1 - mixing_ratio1 / 100)
+    mixed_imaginary = image2_imaginary * (mixing_ratio2 / 100) + image1_imaginary * (1 - mixing_ratio2 / 100)
+
+    mixed_fft = mixed_real + 1j * mixed_imaginary
+    return np.fft.ifft2(mixed_fft).real.astype(np.uint8)
+
+def real_imaginary(mixing_ratio1,mixing_ratio2,comp1,comp2):
+    pass
 
 
 
@@ -84,31 +103,22 @@ def mix_photos(image1, image2, mixing_ratio1,mixing_ratio2,comp1,comp2):
         #miximaginary=image1_imaginary*mixing_ratio1/100+image2_imaginary*mixing_ratio2/100
         #combined = mixreal + miximaginary * 1j
         #mixed_image = np.real(np.fft.ifft2(combined))
-        mixed_real = image1_real * (mixing_ratio1 / 100) + image2_real * (1 - mixing_ratio1 / 100)
-        mixed_imaginary = image2_imaginary * (mixing_ratio2 / 100) + image1_imaginary * (1 - mixing_ratio2 / 100)
+        mixed_image = Uniform_Mag_Phase(mixing_ratio1,mixing_ratio2,image1_real,image2_real,image1_imaginary,image2_imaginary)
 
-        mixed_fft = mixed_real + 1j * mixed_imaginary
-        mixed_image = np.fft.ifft2(mixed_fft).real.astype(np.uint8)
+    elif((comp1 == 'Mag' or comp1 == 'phase' ) and ( comp2 == 'phase' or comp2 =='Mag')):
+        mixed_image = Mag_phase_Mix(mixing_ratio1,mixing_ratio2,magnitude1,magnitude2,phase1,phase2)
+    # elif((comp1 == 'phase' ) and ( comp2 == 'Mag')):
+    #     # mag_mix = magnitude2*(mixing_ratio1/100)+magnitude1*(1-(mixing_ratio1/100))
+    #     # phase_mix = phase2*(1-(mixing_ratio2/100))+phase1*(mixing_ratio2/100)
 
-    elif((comp1 == 'Mag' ) and ( comp2 == 'phase')):
-        mag_mix = magnitude1*(mixing_ratio1/100)+magnitude2*(1-(mixing_ratio1/100))
-        phase_mix = phase1*(1-(mixing_ratio2/100))+phase2*(mixing_ratio2/100)
+    #     # combined_total = np.multiply(mag_mix, np.exp(1j * phase_mix))
+    #     # mixed_image = np.real(np.fft.ifft2(combined_total))
+    #     mag_mix = magnitude2*(mixing_ratio2/100)+magnitude1*(1-(mixing_ratio2/100))
+    #     phase_mix = phase1*((mixing_ratio1/100))+phase2*(1-(mixing_ratio1/100))
 
-        combined_total = np.multiply(mag_mix, np.exp(1j * phase_mix))
-        mixed_image = np.real(np.fft.ifft2(combined_total))
-
-    elif((comp1 == 'phase' ) and ( comp2 == 'Mag')):
-        # mag_mix = magnitude2*(mixing_ratio1/100)+magnitude1*(1-(mixing_ratio1/100))
-        # phase_mix = phase2*(1-(mixing_ratio2/100))+phase1*(mixing_ratio2/100)
-
-        # combined_total = np.multiply(mag_mix, np.exp(1j * phase_mix))
-        # mixed_image = np.real(np.fft.ifft2(combined_total))
-        mag_mix = magnitude2*(mixing_ratio2/100)+magnitude1*(1-(mixing_ratio2/100))
-        phase_mix = phase1*((mixing_ratio1/100))+phase2*(1-(mixing_ratio1/100))
-
-        combined_total = np.multiply(mag_mix, np.exp(1j * phase_mix))
-        mixed_image = np.real(np.fft.ifft2(combined_total))
-        
+    #     combined_total = np.multiply(mag_mix, np.exp(1j * phase_mix))
+    #     mixed_image = np.real(np.fft.ifft2(combined_total))
+    
     elif((comp1 == 'uniphase'  ) and ( comp2 == 'Mag' )):
         mag_mix = magnitude2*(mixing_ratio2/100)+magnitude1*(1-(mixing_ratio2/100))
         phase_mix = uniform_phase1*(mixing_ratio1/100)+phase2*(1-(mixing_ratio1/100))
@@ -119,20 +129,20 @@ def mix_photos(image1, image2, mixing_ratio1,mixing_ratio2,comp1,comp2):
     elif((comp1 == 'Mag'  ) and ( comp2 == 'uniphase' )):
         mag_mix=magnitude1*(mixing_ratio1/100)+magnitude2*(1-(mixing_ratio1/100))
         phase_mix =uniform_phase2*(mixing_ratio2/100)+phase1*(1-(mixing_ratio2/100))
-        
+    
         combined_total = np.multiply(mag_mix, np.exp(1j * phase_mix))
         mixed_image = np.real(np.fft.ifft2(combined_total))
 
     elif(comp1 == 'uniMag' and comp2 == 'phase' ) :
-        mag_mix = uniform_magnitude1*(mixing_ratio1/100)+magnitude2*(1-(mixing_ratio1/100))
+        mag_mix = uniform_magnitude1*(mixing_ratio1/100)+uniform_magnitude2*(1-(mixing_ratio1/100))
         phase_mix = phase2*(mixing_ratio2/100)+phase1*(1-(mixing_ratio2/100))
 
         combined_total = np.multiply(mag_mix, np.exp(1j * phase_mix))
         mixed_image = np.real(np.fft.ifft2(combined_total))
-    
+
     elif(comp1 == 'phase' and comp2 == 'uniMag' ) :
 
-        mag_mix = uniform_magnitude2*(mixing_ratio2/100)+magnitude1*(1-(mixing_ratio2/100))
+        mag_mix = uniform_magnitude2*(mixing_ratio2/100)+uniform_magnitude1*(1-(mixing_ratio2/100))
         phase_mix = phase1*(mixing_ratio1/100)+phase2*(1-(mixing_ratio1/100))
 
         combined_total = np.multiply(mag_mix, np.exp(1j * phase_mix))
@@ -147,10 +157,37 @@ def mix_photos(image1, image2, mixing_ratio1,mixing_ratio2,comp1,comp2):
         return
     #t.write(phase1,phase2)
     mixed_image = (mixed_image - np.min(mixed_image)) / (np.max(mixed_image) - np.min(mixed_image))
-   
+
     return abs(mixed_image)
 
 
 
 
+    # Display images in tkinter window
+    #mag_photo = ImageTk.PhotoImage(magnitude_spectrum)
+    #mag_label.config(image=mag_photo)
+    #mag_label.image = mag_photo
+    #phase_photo = ImageTk.PhotoImage(phase_spectrum)
+    #phase_label.config(image=phase_photo)
+    #phase_label.image = phase_photo
 
+    #ax1 = plt.subplot(1,2,1)
+    #ax1.imshow(img, cmap='gray')
+
+    #ax2 = plt.subplot(1,2,2)
+    #ax2.imshow(phase_spectrum, cmap='gray')
+
+    #plt.show()
+
+    # Display the result
+    #plt.subplot(121),plt.imshow(img, cmap = 'gray')
+    #plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+    #plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
+    #plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+    #plt.show()
+
+#show_Ftmagnitude_and_phase = tk.Button(root, text="show result", command=extract_fourier_characteristics)
+#select_button.pack()
+
+# Start the main event loop to display the window
+#root.mainloop()
